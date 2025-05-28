171ac373d0ac1ea156c6b3ed8dce627f620dba39
SirMengo <msimoes0103@gmail.com>
2025-05-28 12:29:00 +0100

    Completed repeat_alpha

---
+++ Milestone 02/Lvl 01/repeat_alpha/repeat_alpha.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+	int i = 0;
+	int j = 0;
+
+	while(argv[1][i] != '\0'){
+		if(argv[1][i] >= 'a' && argv[1][i] <= 'z')
+			while(j < argv[1][i] - 'a' + 1){
+				write(1, &argv[1][i], 1);
+				j++;
+			}
+		else if(argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			while(j < argv[1][i] - 'A' + 1){
+				write(1, &argv[1][i], 1);
+				j++;
+			}
+		else
+			write(1, &argv[1][i], 1);
+		j = 0;
+		i++;
+	}
+}
