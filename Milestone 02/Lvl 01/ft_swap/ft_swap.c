adc1d09d02cf5bb9da9f483a3e5644296be92fa2
SirMengo <msimoes0103@gmail.com>
2025-05-28 12:04:07 +0100

    Complete strcpy strlen and swap

---
+++ Milestone 02/Lvl 01/ft_swap/ft_swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void ft_swap(int *a, int *b){
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+/*
+int main(){
+	int a = 5;
+	int b = 10;
+
+	ft_swap(&a, &b);
+	printf("%d %d", a, b);
+}
+*/
